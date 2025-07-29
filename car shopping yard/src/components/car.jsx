c2b8a0e48886864411ed6dec35ff83951e02d222
Levi Mutai <54dev.bylevi@gmail.com>
2025-07-30 02:30:42 +0300

    Add car listing functionality

--- car shopping yard/src/components/car.jsx
+++ car shopping yard/src/components/car.jsx
@@ -3,3 +3,10 @@ import CarCard from '../components/CarCard';
 import CarDetail from '../components/CarDetail';
 import SearchBar from '../components/SearchBar';
 import '../styles/Cars.css';
+
+function Cars() {
+  const [cars, setCars] = useState([]);
+  const [filteredCars, setFilteredCars] = useState([]);
+  const [selectedCar, setSelectedCar] = useState(null);
+  const [loading, setLoading] = useState(true);
+  
